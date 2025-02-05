0370c98c099e28f22d04dcbd99c3f23025228b34
AbdulrahmanAlShaweesh <abdulrahmanalshwish2022@gmail.com>
2025-02-06 01:57:52 +0300

    Added Exam Class

--- C#Projects/ExaminationSystem/ExaminationSystem/Classes/Exam.cs
+++ C#Projects/ExaminationSystem/ExaminationSystem/Classes/Exam.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace ExaminationSystem.Classes
 {
-    internal class Exam
+    internal abstract class Exam    // based class for all exams type : final , and practical
     {
+        public int TimeOfExam { get; set; }
+        public int NumberOfQuestions { get; set; }
 
+        public abstract void ShowExam();  // we need to impelement this method in each class it inherit
+                                          // from exam class.
     }
 }
