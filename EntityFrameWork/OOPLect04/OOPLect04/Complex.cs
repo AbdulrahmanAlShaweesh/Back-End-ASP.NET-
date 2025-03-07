4aab9c971a6d1297adf63df125485c0ffd81056e
AbdulrahmanAlShaweesh <abdulrahmanalshwish2022@gmail.com>
2025-03-07 15:42:51 +0300

    added Folder for Database

---
+++ EntityFrameWork/OOPLect04/OOPLect04/Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLect04
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Image { get; set; }
+
+        //Note : we have to have both functions
+        public static bool operator > (Complex left, Complex right)
+        {
+            if(left?.Real == right?.Real)
+            {
+                return left?.Image > right?.Image;
+            }else
+            {
+                return left?.Real > right?.Real;
+            }
+        }
+
+        public static bool operator < (Complex left, Complex right)
+        {
+            if (left?.Real < right?.Real)
+            {
+                return left?.Image < right?.Image;
+            }
+            else
+            {
+                return left?.Real < right?.Real;
+            }
+        }
+    }
+}
