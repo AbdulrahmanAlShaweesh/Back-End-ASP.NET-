310ffb20091838d264a0bd40e9a6dd012774f312
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2025-04-14 17:42:34 +0300

    Updated Folder

--- C#ProblemSolving/OOPs/OOP L06/Thired Project OOP L06/OOP L06/Discounts/Discount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Third_Project.Discounts
-{
-    internal abstract class Discount
-    {
-        public abstract string Name { get; }
-
-        public abstract double CalculateDiscount(double price, int quantity);
-    }
-}
--- MVC/ClothingStoreManagement/ClothingStore.DataAccess/GlobalUsing.cs
+++
@@ -1,6 +0,0 @@
-﻿global using ClothingStore.DataAccess.Models;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-
- 
