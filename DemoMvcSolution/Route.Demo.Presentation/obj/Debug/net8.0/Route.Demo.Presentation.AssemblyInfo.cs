c7ce4dbfb139b59c7631c053c4233e0b071ec142
abdulrahmanalshwish <abdulrahmanalshwish2022@gmail.com>
2025-04-18 19:36:00 +0300

    AntiForgeryToken

--- DemoMvcSolution/Route.Demo.Presentation/obj/Debug/net8.0/Route.Demo.Presentation.AssemblyInfo.cs
+++ DemoMvcSolution/Route.Demo.Presentation/obj/Debug/net8.0/Route.Demo.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Route.Demo.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c8e17e7ea13adbb5d12881b49523791cf02323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1884683b207e968165eb473122dc7103fa709e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Route.Demo.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Route.Demo.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
